82cebac9a31d3e843fe706fb7a1a6346b9609176
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-09-30 00:21:07 +0800

    Get lat, long from google maps API and get distance to MRT via geopy

--- distlabel.py
+++ distlabel.py
@@ -1,11 +1,44 @@
 from geopy.geocoders import GoogleV3
+import geopy.distance
+from openpyxl import load_workbook, Workbook
 
 with open('API_KEY.txt','r') as API:
     API_KEY = API.read()
 
 # need to enable Geocoding API in Google Cloud Platform first
 geolocator = GoogleV3(API_KEY)
-print(type(geolocator))
 
-location = geolocator.geocode('105 BT BATOK CTRL')
-print(location.latitude, location.longitude)
+# Tampines MRT lat: 1.3533834 long: 103.9451538
+tampines_mrt = (1.3533834, 103.9451538)
+
+# Load the Workbook
+wb = load_workbook('resale_sample.xlsx')
+# Load the Worksheet
+sheet = wb['Sheet1']
+
+addr_list = []
+# ' ' is padding for index purposes
+dist_list = [' ','distance']
+
+# start from 2 to avoid the column headers, range() is one less step than max
+for i in range(2, sheet.max_row + 1):
+    block = str(sheet.cell(row=i, column=4).value)
+    street_name = sheet.cell(row=i, column=5).value
+    addr_list.append(block + ' ' + street_name)
+
+addr_list_len = len(addr_list)
+
+for index, address in enumerate(addr_list):
+    print(str(index+1)+'/'+str(addr_list_len))
+    location = geolocator.geocode(address)
+    place = (location.latitude, location.longitude)
+    dist =geopy.distance.geodesic(place, tampines_mrt).km
+    dist_list.append(dist)
+
+dist_list_len = len(dist_list)
+
+for row in range(1,dist_list_len):
+    cell = sheet.cell(column=11, row=row)
+    cell.value = dist_list[row]
+
+wb.save('resale_sample.xlsx')
