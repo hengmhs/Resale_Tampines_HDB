34e70bebd398adb12a84f676e12c6c5c374ad662
hengmhs <31433170+hengmhs@users.noreply.github.com>
2021-09-30 19:44:01 +0800

    Add labels for Upper Changi MRT, Simei MRT and update map visualisation

--- blue_distlabel.py
+++ blue_distlabel.py
@@ -1,10 +1,13 @@
 import geopy.distance
-from openpyxl import load_workbook, Workbook
+from openpyxl import load_workbook, Workbook, utils
 
-tampines_east_mrt = (1.3558290924997582, 103.95520090009265)
-tampines_west_mrt = (1.345808393511592, 103.9382382867281)
+# tampines_east_mrt = (1.3558290924997582, 103.95520090009265)
+# tampines_west_mrt = (1.345808393511592, 103.9382382867281)
 
 wb_name = 'resale2013_2018.xlsx'
+
+#simei_mrt = (1.343097244672535, 103.95331048465245)
+upper_changi_mrt = (1.34164302491625, 103.96146762682966)
 
 # Load the Workbook
 wb = load_workbook(wb_name)
@@ -12,8 +15,8 @@ wb = load_workbook(wb_name)
 sheet = wb['Sheet1']
 
 coord_list = []
-east_dist_list = []
-west_dist_list = []
+simei_dist_list = []
+changi_dist_list = []
 
 # start from 2 to avoid the column headers, range() is one less step than max
 # get coordinates of each place
@@ -36,15 +39,15 @@ for i in range(0, len(east_dist_list)):
     cell.value = east_dist_list[i]
 '''
 
-# get distances between coord and tampines west mrt
+# get distances between coord and simei mrt
 for index, coord in enumerate(coord_list):
     print(str(index+1)+'/'+str(len(coord_list)),end='\r')
-    dist = geopy.distance.geodesic(coord, tampines_west_mrt).km
-    west_dist_list.append(dist)
+    dist = geopy.distance.geodesic(coord, upper_changi_mrt).km
+    changi_dist_list.append(dist)
 
-# write tampines west mrt to excel
-for i in range(0, len(west_dist_list)):
-    cell = sheet.cell(column=15, row=i+2)
-    cell.value = west_dist_list[i]
+# write simei mrt to excel
+for i in range(0, len(changi_dist_list)):
+    cell = sheet.cell(column=utils.column_index_from_string('Q'), row=i+2)
+    cell.value = changi_dist_list[i]
 
 wb.save(wb_name)
--- map.py
+++ map.py
@@ -1,5 +1,5 @@
 import gmplot
-from openpyxl import load_workbook, Workbook
+from openpyxl import load_workbook, Workbook, utils
 
 wb_name = 'resale2013_2018.xlsx'
 
@@ -13,25 +13,30 @@ with open('API_KEY.txt','r') as API:
 
 gmap = gmplot.GoogleMapPlotter(1.3533834, 103.9451538, 15, apikey=API_KEY)
 
-coord_list = []
-# list of hdbs near tampines MRT
-tamp_list = []
+
+coord_list = [] # list of all hdbs not near MRT
+tamp_list = [] # list of hdbs near tampines MRT
+other_list = [] # list of hdbs near another MRT that is not tampines
 
 # start from 2 to avoid the column headers, range() is one less step than max
 # get coordinates of each place
 for i in range(2, sheet.max_row + 1):
-    lat = sheet.cell(row=i, column=11).value
-    longitude = sheet.cell(row=i, column=12).value
+    lat = sheet.cell(row=i, column=utils.column_index_from_string('K')).value
+    longitude = sheet.cell(row=i, column=utils.column_index_from_string('L')).value
     coord = (lat, longitude)
     # get value of tampines_mrt, east_mrt, west_mrt
-    tampines_mrt = sheet.cell(row=i,column=16).value
-    east_mrt = sheet.cell(row=i,column=17).value
-    west_mrt = sheet.cell(row=i,column=18).value
+    tampines_mrt = sheet.cell(row=i,column=utils.column_index_from_string('R')).value
+    east_mrt = sheet.cell(row=i,column=utils.column_index_from_string('S')).value
+    west_mrt = sheet.cell(row=i,column=utils.column_index_from_string('T')).value
+    simei_mrt = sheet.cell(row=i,column=utils.column_index_from_string('U')).value
+    upper_changi_mrt = sheet.cell(row=i,column=utils.column_index_from_string('V')).value
     # check if tampines_mrt = 1
     if tampines_mrt:
         tamp_list.append(coord)
-    # check if coord is near east_mrt or west_mrt
-    elif not east_mrt and not west_mrt :
+    # check if coord is near other mrts
+    elif east_mrt or west_mrt or simei_mrt or upper_changi_mrt:
+        other_list.append(coord)
+    else:
         coord_list.append(coord)
 
 # removes duplicates
@@ -54,6 +59,16 @@ coord_list = list(coord_set)
 coord_list_lat = []
 coord_list_long = []
 
+other_set = set(other_list) # removes duplicates
+other_list = list(other_set) # convert set back to list
+
+other_list_lat = []
+other_list_long = []
+
+for coord in other_list:
+    other_list_lat.append(coord[0])
+    other_list_long.append(coord[1])
+
 for coord in coord_list:
     coord_list_lat.append(coord[0])
     coord_list_long.append(coord[1])
@@ -61,10 +76,19 @@ for coord in coord_list:
 # scatter takes a list of lats and longs
 gmap.scatter(tamp_list_lat,tamp_list_long,size=8,color='red',marker=False,alpha=1)
 gmap.scatter(coord_list_lat, coord_list_long,size=8,color='blue',marker=False)
+gmap.scatter(other_list_lat, other_list_long,size=8,color='green',marker=False)
 
+# draw Tampines MRT Circle
 gmap.circle(1.3533834, 103.9451538,400,face_alpha=0,edge_color='red')
-gmap.circle(1.3558290924997582, 103.95520090009265,400,face_alpha=0,edge_color='blue')
-gmap.circle(1.345808393511592, 103.9382382867281,400,face_alpha=0,edge_color='blue')
-gmap.circle(1.3433117614470864, 103.95364307656335,400,face_alpha=0,edge_color='blue')
-gmap.circle(1.3418146392239008, 103.96183240725058,400,face_alpha=0,edge_color='blue')
+
+# Tampines West
+gmap.circle(1.3558290924997582, 103.95520090009265,400,face_alpha=0,edge_color='green')
+# Tampines East
+gmap.circle(1.345808393511592, 103.9382382867281,400,face_alpha=0,edge_color='green')
+# Simei
+gmap.circle(1.343097244672535, 103.95331048465245,400,face_alpha=0,edge_color='green')
+# Upper Changi
+gmap.circle(1.34164302491625, 103.96146762682966,400,face_alpha=0,edge_color='green')
+
+# output the visualisation
 gmap.draw('visualisation.html')
